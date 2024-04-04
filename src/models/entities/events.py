c01522339ada9d8b4685228b92ddb549f7de8c1e
Diego <diegodsjoker@gmail.com>
2024-04-04 16:07:27 -0300

    feat: Testing classes

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -6,11 +6,14 @@ class Events(Base):
 
     id = Column(String, primary_key=True)
     title = Column(String,nullable=False)
-    detais = Column(String)
+    details = Column(String)
     slug = Column(String,nullable=False)
     maximum_attendees = Column(Integer)
 
-# class attendees(Base):
+    def __repr__(self):
+        return f"Events [title={self.title}, slug={self.slug}, maximum_attendees={self.maximum_attendees}]"
+    
+# class Attendees(Base):
 #     __tablename__ = 'attendees'
 
 #     id = Column(String, primary_key=True)
